87633be179bceac10c787fa28c524ea000228e4d
Dmitry Nefedov <nefedovds@ya.ru>
2026-02-27 00:32:02 +0300

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,11 +1,18 @@
 #Requires -RunAsAdministrator
 #Requires -Version 5.1
 
-if (Test-Path -Path $PSScriptRoot\Office\Data\*\stream.x64.x-none.dat)
+if (-not (Test-Path -Path $PSScriptRoot\Office\Data\*\stream.x64.x-none.dat))
 {
-	Start-Process -FilePath "$PSScriptRoot\setup.exe" -ArgumentList "/configure `"$PSScriptRoot\Config.xml`"" -Wait
-}
-else
-{
+	Write-Information -MessageData "" -InformationAction Continue
 	Write-Verbose -Message "There aren't neccessary Office files to install. Please do not move Install.ps1 from where all files are." -Verbose
 }
+
+if (-not (Test-Path -Path $PSScriptRoot\Config.xml))
+{
+	Write-Information -MessageData "" -InformationAction Continue
+	Write-Verbose -Message "There is no Config.xml in $PSScriptRoot." -Verbose
+}
+
+Write-Information -MessageData "" -InformationAction Continue
+Write-Verbose -Message "Installing downloaded Microsoft 365 components..." -Verbose
+Start-Process -FilePath "$PSScriptRoot\setup.exe" -ArgumentList "/configure `"$PSScriptRoot\Config.xml`"" -Wait
